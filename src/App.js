4a77c2b7adbf9955c0b877566d8c12c25684ae43
walde73 <75583669+walde73@users.noreply.github.com>
2021-09-15 23:39:03 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import './styles.css';
+
+const useSortableData = (items, config = null) => {
+  const [sortConfig, setSortConfig] = React.useState(config);
+
+  const sortedItems = React.useMemo(() => {
+    let sortableItems = [...items];
+    if (sortConfig !== null) {
+      sortableItems.sort((a, b) => {
+        if (a[sortConfig.key] < b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? -1 : 1;
+        }
+        if (a[sortConfig.key] > b[sortConfig.key]) {
+          return sortConfig.direction === 'ascending' ? 1 : -1;
+        }
+        return 0;
+      });
+    }
+    return sortableItems;
+  }, [items, sortConfig]);
+
+  const requestSort = (key) => {
+    let direction = 'ascending';
+    if (
+      sortConfig &&
+      sortConfig.key === key &&
+      sortConfig.direction === 'ascending'
+    ) {
+      direction = 'descending';
+    }
+    setSortConfig({ key, direction });
+  };
+
+  return { items: sortedItems, requestSort, sortConfig };
+};
+
+const ProductTable = (props) => {
+  const { items, requestSort, sortConfig } = useSortableData(props.products);
+  const getClassNamesFor = (name) => {
+    if (!sortConfig) {
+      return;
+    }
+    return sortConfig.key === name ? sortConfig.direction : undefined;
+  };
+  return (
+    <table>
+      <caption>Sort by</caption>
+      <thead>
+        <tr>
+          <th>
+            <button
+              type="button"
+              onClick={() => requestSort('name')}
+              className={getClassNamesFor('name')}
+            >
+              Name
+            </button>
+          </th>
+          <th>
+            <button
+              type="button"
+              onClick={() => requestSort('price')}
+              className={getClassNamesFor('price')}
+            >
+              Price
+            </button>
+          </th>
+          <th>
+            <button
+              type="button"
+              onClick={() => requestSort('Status')}
+              className={getClassNamesFor('Status')}
+            >
+              Status
+            </button>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map((item) => (
+          <tr key={item.id}>
+            <td>{item.name}</td>
+            <td>${item.price}</td>
+            <td>{item.Status}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <ProductTable
+        products={[
+          { id: 1, name: 'Tunde', price: 4.9, Status: 'Pending' },
+          { id: 2, name: 'Gabriel', price: 1.9, Status: 'Approved' },
+          { id: 3, name: 'Busayo', price: 2.4, Status: 'Approved' },
+          { id: 4, name: 'Abimbola', price: 3.9, Status: 'Approved' },
+          { id: 5, name: 'Tope', price: 0.9, Status: 'Pending' },
+          { id: 6, name: 'Elvis ', price: 2.9, Status: 'Approved' },
+          { id: 7, name: 'Mark', price: 99, Status: 'Pending' },
+        ]}
+      />
+    </div>
+  );
+}
